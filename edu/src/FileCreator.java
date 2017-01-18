2249b9710107ccdab6a7731d335c255316ba3cac
roman <romanandreevich7@gmail.com>
2017-01-18 18:17:01 +0300

    1

---
+++ edu/src/FileCreator.java
@@ -0,0 +1,26 @@
+/**Код должен создавать файл, имя - текущаяя дата и время. 
+ * 
+ */
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+public class FileCreator {
+	public static String fileCreate() {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yy HH_mm_ss");
+		String filename = sdf.format(new Date()) + ".doc";
+		String absolutePath = "/home/roman/git/edu/edu/logs";
+		File file = new File(absolutePath, filename);
+		try {
+			file.createNewFile();			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		String s = file.getName();
+		return s;
+	}
+
+}
