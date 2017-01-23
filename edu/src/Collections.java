e1ebf9b9da26a858c92acf0ec206065c9bff827d
roman <romanandreevich7@gmail.com>
2017-01-23 17:58:49 +0300

    small changes

--- edu/src/Collections.java
+++ edu/src/Collections.java
@@ -3,11 +3,11 @@ import java.util.Map;
 
 
 public class Collections {
-	public static String str1 = Grade.gradeling.Moscow.toString();
+	
 	public static String createMap() {
 		Map<String, String> data = new HashMap <String, String> ();
 		data.put("Moscow", "777");
-		String value = data.get(str1);
+		String value = data.get(Grade.gradeling.Moscow.toString());
 		return value;
 	}
 
